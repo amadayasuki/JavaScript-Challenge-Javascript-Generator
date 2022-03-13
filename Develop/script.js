3d8478946f22729120b27a019c482d99d2edda08
Sensess <SensessYentzer@outlook.com>
2022-03-13 13:26:11 -0400

    function length

---
+++ Develop/script.js
@@ -0,0 +1,30 @@
+var generateBtn = document.querySelector("#generate");
+var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberChar = "0123456789";
+var specialChar = "!@#$%^&*()_-+={}[];:'`~<,>.?/|"
+var passwordLength;
+var uppercaseCheck;
+var numberCheck;
+var specialCheck;
+
+//Function used to determine the length of the password
+function determineLength(){
+  passwordLength = prompt("How many characters would you like in your password? (Between 8-128)");
+
+    if (passwordLength<8){
+      alert("Password length must be a number between 8-128 characters");
+      determineLength();
+    }else if (passwordLength>128){
+      alert("Password length must be a number between 8-128 characters");
+      determineLength();
+    }else if (isNaN(passwordLength)){
+      alert("Password length must be a number between 8-128 characters");
+      determineLength();
+    }
+    return passwordLength;
+}
+
+//
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
